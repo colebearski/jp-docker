467618b5485fbc4061459937eeac3bd68412f3b4
Cole Pedersen <colepedersen@CP.local>
2018-09-21 15:05:35 -0600

    create pages api

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,12 +2,13 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---src-templates-blog-template-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/templates/blog-template.js" /* webpackChunkName: "component---src-templates-blog-template-js" */),
   "component---cache-dev-404-page-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-blog-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
   "component---src-pages-index-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
-  "component---src-pages-blog-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */)
+  "component---src-pages-page-2-js": () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
 }
 
 exports.data = () => import("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/.cache/data.json")
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,11 +3,12 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---src-templates-blog-template-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/templates/blog-template.js")),
   "component---cache-dev-404-page-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/about.js")),
+  "component---src-pages-blog-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/blog.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/page-2.js")),
-  "component---src-pages-blog-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/blog.js"))
+  "component---src-pages-page-2-js": preferDefault(require("/Users/colepedersen/Desktop/snowbasin/Code/Product/jp-docker/src/pages/page-2.js"))
 }
 
